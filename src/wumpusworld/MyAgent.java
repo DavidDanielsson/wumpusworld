71e3c6bdd658ac38e7877e3f0dcc35bb6f22b395
unknown <BTH@H470-0001.students.ad.bth.se>
2016-10-13 15:14:20 +0200

    Now read utility from file with bitmask and cool stuff

--- src/wumpusworld/MyAgent.java
+++ src/wumpusworld/MyAgent.java
@@ -7,6 +7,11 @@ package wumpusworld;
  * @author Johan Hagelbäck
  */
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 
 /**
@@ -81,6 +86,9 @@ public class MyAgent implements Agent
             , { 0, -1 }
         };
 
+
+        // Open list expands around the player each time he makes a move.
+        // Nodes are only removed when player moves to them.
         for(int i = 0; i < 4; ++i)
         {
             int nextX = cX + offset[i][0];
@@ -95,10 +103,25 @@ public class MyAgent implements Agent
             }
         }
 
+
         // Open list is updated. Iterate through it
-            // generate "hash" for board surrounding current tile
+        int maxUtility = Integer.MIN_VALUE;
+        Tile bestTile;
+        for (Tile tile:openList)
+        {
+            int utility = GetUtility(tile);
+            if(utility > maxUtility)
+            {
+                maxUtility = utility;
+                bestTile = tile;
+            }
+            maxUtility = Integer.max(maxUtility, GetUtility(tile));
+        }
+
+        // We now have the best tile.
+        // Move the player
+
             // Flip wumpus bits if he is found
-            // Lookup board "hash" and gather utility number
         // List iterated. Make move with best utility
         // Once move is done, update utility
 
@@ -166,7 +189,7 @@ public class MyAgent implements Agent
         //    w.doAction(World.A_TURN_RIGHT);
         //    w.doAction(World.A_MOVE);
         //}
-                
+
     }    
     
      /**
@@ -176,7 +199,99 @@ public class MyAgent implements Agent
     {
       return (int)(Math.random() * 4);
     }
-    
+
+    private static final int numStuffWeTrack = 2;
+    private static final int breeze = 1;
+    private static final int stench = 2;
+    private static final int unknown = 4;
+
+
+
+    private int GetUtility(Tile tile)
+    {
+        // Build bit mask for surrounding board
+        int bitMask = 1 << 18;
+        for (int y = -1; y < 2; y++)
+            for(int x = -1; x < 2;x++)
+            {
+                int worldTileX = x + tile.x;
+                int worldTileY = y + tile.y;
+
+                if(w.hasBreeze(worldTileX, worldTileY))
+                {
+                    // Hax
+                    bitMask |= breeze << (numStuffWeTrack * ((x + 1) + (y + 1) * 3));
+                }
+                if(w.hasStench(worldTileX,worldTileY))
+                {
+                    // Hax
+                    bitMask |= stench << (numStuffWeTrack * ((x + 1) + (y + 1) * 3));
+                }
+                // Might be worth tracking unknown and out of bounds
+                //if(w.isUnknown(worldTileX,worldTileY))
+                //{
+                //    // Hax
+                //    bitMask |= unknown << (3 * ((x + 1) + (y + 1) * 3));
+                //}
+                // Maybe check if we're out of map
+            }
+        // Debug string for us to visualize
+        String bitMaskString = Integer.toBinaryString(bitMask);
+        // We now have our bit mask. Look up utility value in file
+        int utilityValue = GetUtilityFromFile(bitMask);
+
+        return utilityValue;
+    }
+
+    private int GetUtilityFromFile(int bitMask)
+    {
+        final String databaseFileName = "database.txt";
+        //File file = new File("database.txt"); // pleb version
+        // Read all lines and save into array list
+        ArrayList<String> lines = new ArrayList<>();
+        String line;
+        // open op file and start reading
+        try (
+                InputStream fis = new FileInputStream(databaseFileName);
+                InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+                BufferedReader br = new BufferedReader(isr);
+        )
+        {
+            // Read all lines from the file
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+            // Catch because we need it
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // now we have an array list of a bunch of lines
+        for (String readLine:lines)
+        {
+            // Split line into bitmask and value
+            String[] splitLine = readLine.split("-");
+            int parsedInt = Integer.parseInt(splitLine[0]);
+            // See if this is the bitmask we're looking for
+            if(parsedInt == bitMask)
+            {
+                int parsedVal = Integer.parseInt(splitLine[1]);
+                return parsedVal;
+            }
+        }
+        // This board was previously unencountered. Add it to database
+        String writeString = Integer.toString(bitMask) + "-0";
+        try {
+            Files.write(Paths.get(databaseFileName), writeString.getBytes(), StandardOpenOption.APPEND);
+        }catch (IOException e) {
+            //exception handling left as an exercise for the reader
+        }
+
+        return 0;
+    }
+
     
 }
 
